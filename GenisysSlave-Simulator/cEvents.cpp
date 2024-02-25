183a29c6bfd3a0502741e5e2eadf95f72b960c09
LSY <LSY@>
2024-02-25 16:06:19 +1100

    Start Add Gensisy Network Code To wxWidgets GUI, Still Working Out To Get Around recvfrom Call Blocking.

--- GenisysSlave-Simulator/cEvents.cpp
+++ GenisysSlave-Simulator/cEvents.cpp
@@ -8,10 +8,81 @@ using namespace LSY;
 
 cEvents::cEvents() : cFrame(nullptr)
 {
+	// UI States
+	start_stop_server_request_ = m_toggleStartStop->GetValue();
+	udp_rx_port_ = -1;
+	udp_tx_port_ = -1;
+	genisys_slave_id_ = -1;
+	genisys_frame_size_ = -1;
+
+	bit_write_request_ = false;
+	byte_offset_ = -1;
+	bit_offset_ = -1;
+	bit_value_ = false;
+
+
+	// Genisys Slave
+	data_frame_1_ = nullptr;
+	protocol_ = nullptr;
+	network_ = nullptr;
+
+
+	// Setup Logging
+	logger = new wxLogWindow(this, "Log", true, false);
+	wxLog::SetActiveTarget(logger);
+	wxLogNull no_log;
+	
 
 	// Connect Idle Event
 	this->Bind(wxEVT_IDLE, &cEvents::IdleEvent, this);
+}
 
+
+void cEvents::StartStopServerEvent(wxCommandEvent& event)
+{
+
+	if (m_toggleStartStop->GetValue() == true) // Turn On Genisys Server
+	{
+		if (StartupGenisysNetwork())
+		{
+			// Enable User Controls On The Grid
+			m_grid3->Enable(true);
+
+			// Setup Grid Size Based On Genisys Slave DB Bits
+			m_grid3->DeleteRows(0, m_grid3->GetNumberRows());
+			m_grid3->AppendRows(genisys_frame_size_);
+
+			// Setup Grid Values Based On Genisys Slave DB Bits
+			// Note: For Now Just Assume 0
+			for (int i = 0; i < genisys_frame_size_; i++)
+			{
+				for (int j = 0; j < 8; j++)
+				{
+					m_grid3->SetCellValue(wxGridCellCoords(i, j), "0");
+					m_grid3->SetCellBackgroundColour(i, j, *wxRED);
+				}
+			}
+
+		}
+
+		else
+		{
+			// Disable User Controls On The Grid
+			m_grid3->Enable(false);
+
+			// Set Start/Stop Button State
+			m_toggleStartStop->SetValue(false);
+		}
+	}
+
+
+	else
+	{
+		ShutdownGenisysNetwork();
+	}
+
+
+	event.Skip();
 }
 
 void cEvents::GridClickEvent(wxGridEvent& event)
@@ -19,116 +90,53 @@ void cEvents::GridClickEvent(wxGridEvent& event)
 	// Get Coord Clicked
 	wxGridCellCoords coord = wxGridCellCoords(event.GetRow(), event.GetCol());
 
-	// Flip Value
+	// Get Value
 	wxString value = m_grid3->GetCellValue(coord);
+
+	// Request To Genisys Thread To Change A Bit Value
+	if (value == wxString("1"))
+		bit_value_ = false;
+	else
+		bit_value_ = true;
+	byte_offset_ = coord.GetRow();
+	bit_offset_ = coord.GetCol();
+	bit_write_request_ = true;
+
+
+
+
+	// Old Code
 	if (value == wxString("1"))
 	{
-		m_grid3->SetCellValue(coord, wxString("0"));
-		m_grid3->SetCellBackgroundColour(coord.GetRow(), coord.GetCol(), *wxRED);
-	}	
+		//m_grid3->SetCellValue(coord, wxString("0"));
+		//m_grid3->SetCellBackgroundColour(coord.GetRow(), coord.GetCol(), *wxRED);
+	}
 	else
 	{
-		m_grid3->SetCellValue(coord, wxString("1"));
-		m_grid3->SetCellBackgroundColour(coord.GetRow(), coord.GetCol(), *wxGREEN);
-	}	
+		//m_grid3->SetCellValue(coord, wxString("1"));
+		//m_grid3->SetCellBackgroundColour(coord.GetRow(), coord.GetCol(), *wxGREEN);
+	}
+
+
 
 	event.Skip();
 }
 
-void cEvents::StartStopServerEvent(wxCommandEvent& event)
-{
 
-	if (m_toggleStartStop->GetValue() == true)
-	{
-
-		// Get UDP Reciving Port
-		wxString udp_rx = m_textUDPrx->GetValue();
-
-		// Get UPD Transmit Port
-		wxString udp_tx = m_textUDPtx->GetValue();
-		
-		// Get Slave ID
-		wxString gen_slave_id = m_textSlaveID->GetValue();
-		
-		// Get Frame Size
-		unsigned int frame_size = 0;
-		m_textFrameSize->GetValue().ToUInt(&frame_size);
-		if (frame_size > 125)
-		{
-			frame_size = 125;
-			m_textFrameSize->SetValue("125");
-		}
-			
-
-
-
-		// Spin Up A Genisys UDP Slave
-
-		// Data Frame Setup
-		std::shared_ptr<DataFrame> data_1 = std::make_shared<DataFrame>("Genisys Slave 1", frame_size);
-
-		// Protocol Setup
-		std::shared_ptr<ProtocolGenisysSlave> protocol = std::make_shared<ProtocolGenisysSlave>();
-		protocol->AddDataFrame(1, data_1);
-
-		// Network Configuration
-		NetworkGenisysSlave::Config network_config;
-		network_config.server_port_ = 7169;
-		network_config.destination_port_ = 7169;
-
-		// Network Setup
-		NetworkGenisysSlave network(network_config);
-		network.AddProtocol(protocol);
-
-		// ------- Run Network Server ---------
-		network.StartServer();
-		//network.ServerLoop();
-
-
-
-
-		// Enable User Controls On The Grid
-		m_grid3->Enable(true);
-
-		// Setup Grid Size Based On Genisys Slave DB Bits
-		m_grid3->DeleteRows(0, m_grid3->GetNumberRows());
-		m_grid3->AppendRows(frame_size);
-
-		// Setup Grid Values Based On Genisys Slave DB Bits
-		// Note: For Now Just Assume 0
-		for (int i = 0; i < frame_size; i++)
-		{
-			for (int j = 0; j < 8; j++)
-			{
-				m_grid3->SetCellValue(wxGridCellCoords(i, j), "0");
-				m_grid3->SetCellBackgroundColour(i, j, *wxRED);
-			}
-		}
-	}
-
-	else
-	{
-		// Disable User Controls On The Grid
-		m_grid3->Enable(false);
-
-		// Shutdown Genisys UDP Slave
-
-	}
-
-
-	event.Skip();
-}
 
 void cEvents::MenuHelpAboutOnMenuSelection(wxCommandEvent& event)
 {
 
 	std::string msg = "";
-	msg += "Genisys Application\n";
+	msg += "About - Genisys UDP Slave Application\n";
 	msg += "\n";
 	msg += "Author: Luke Young\n";
 	msg += "GitHub: LukeYoung3000\n";
+	msg += ("Version: " + std::string(version) + "\n");
 
 	wxMessageBox(wxString(msg), "About", wxOK | wxICON_INFORMATION);
+
+	logger->Show();
 
 	event.Skip();
 }
@@ -138,5 +146,147 @@ void cEvents::IdleEvent(wxIdleEvent& event)
 {
 	//wxMessageBox("Yo", "IdleEvent Without Skip", wxOK | wxICON_INFORMATION);
 	//m_toggleStartStop->SetValue(!m_toggleStartStop->GetValue());
+	static int i = 0;
+	i++;
+	wxLogMessage("Idle Event %d", i);
+
 	event.Skip();
+}
+
+
+
+
+bool cEvents::StartupGenisysNetwork()
+{
+
+	// Check Server Thread Is NOT Running
+	if (network_thread_.joinable())
+	{
+		// Error: Thread Is Already Running
+		ShutdownGenisysNetwork();
+		return false;
+	}
+
+	// Get Parameters From GUI
+	bool int_check = true;
+	int_check = int_check && m_textUDPrx->GetValue().ToInt(&udp_rx_port_);					// Get UDP Reciving Port
+	int_check = int_check && m_textUDPtx->GetValue().ToInt(&udp_tx_port_);					// Get UPD Transmit Port
+	int_check = int_check && m_textSlaveID->GetValue().ToInt(&genisys_slave_id_);			// Get Genisys Slave ID
+	int_check = int_check && m_textFrameSize->GetValue().ToInt(&genisys_frame_size_);		// Get Genisys Frame Size
+
+	if (int_check == false)
+	{
+		// Error: Invalid Data Input
+		ShutdownGenisysNetwork();
+		return false;
+	}
+
+	// Max Frame Size Is Hardcoded At 125
+	if (genisys_frame_size_ > 125)
+	{
+		genisys_frame_size_ = 125;
+		m_textFrameSize->SetValue("125");
+	}
+
+
+
+
+	// Setup Genisys UDP Slave
+
+	// Data Frame Setup
+	data_frame_1_ = std::make_shared<DataFrame>("Genisys Slave 1", genisys_frame_size_);
+
+	// Protocol Setup
+	protocol_ = std::make_shared<ProtocolGenisysSlave>();
+	if (!protocol_->AddDataFrame(genisys_slave_id_, data_frame_1_))
+	{
+		// Error: Data Frame Issue
+		ShutdownGenisysNetwork();
+		return false;
+	}
+
+	// Network Configuration
+	NetworkGenisysSlave::Config network_config_;
+	network_config_.server_port_ = udp_rx_port_;
+	network_config_.destination_port_ = udp_tx_port_;
+	network_ = std::make_shared<NetworkGenisysSlave>(network_config_);
+	if (!network_->AddProtocol(protocol_))
+	{
+		// Error: Protocol Issues
+		ShutdownGenisysNetwork();
+		return false;
+	}
+
+	// Start Server
+	if (!network_->StartServer())
+	{
+		ShutdownGenisysNetwork();
+		return false;
+	}
+	
+	
+	// Setup Thread For Main Loop
+
+
+
+
+
+}
+
+
+void cEvents::ShutdownGenisysNetwork()
+{
+
+	// Check If Thread Is Active
+	if (network_thread_.joinable())
+	{
+		// Shutdown Thread
+		stop_thread_ = true;
+		network_thread_.join();
+		stop_thread_ = false;
+	}
+
+
+	data_frame_1_.reset();
+	protocol_.reset();
+	network_.reset();
+
+	bit_write_request_ = false;
+
+}
+
+
+void cEvents::RunningloopGenisysNetwork()
+{
+
+	while (true)
+	{
+		
+		if (stop_thread_)
+		{
+			return;
+		}
+
+
+		if (bit_write_request_)
+		{
+			if (!data_frame_1_->WriteBit(byte_offset_, bit_offset_, bit_value_))
+			{
+				// Error: Bit Failed To Write
+			}
+			else
+			{
+				// Update The wxWidgets GUI
+			}
+		}
+
+
+		network_->ServerLoop();
+
+	}
+
+
+
+
+
 }
--- GenisysSlave-Simulator/cEvents.h
+++ GenisysSlave-Simulator/cEvents.h
@@ -1,20 +1,60 @@
+
+// STL
+#include <utility>
+#include <atomic>
+#include <thread>
+
+// wxWidgets
+#include <wx/event.h>
+#include <wx/log.h>
+
+// Genisys Lib
+#include "NetworkGenisysSlave.h"
+
+// Genisys Slave Simulator
+#include "Version.h"
 #include "cFrame.h"
 
-#include <wx/event.h>
 
 class cEvents : public cFrame
 {
 public:
 
+	// UI States
+	std::atomic<bool> start_stop_server_request_;
+	int udp_rx_port_;
+	int udp_tx_port_;
+	int genisys_slave_id_;
+	int genisys_frame_size_;
+
+	std::atomic<bool> bit_write_request_;
+	std::atomic<int> byte_offset_;
+	std::atomic<int> bit_offset_;
+	std::atomic<bool> bit_value_;
+
+	// Logging
+	wxLogWindow* logger;
+
+	// Genisys Network
+	std::atomic<bool> stop_thread_;
+	std::thread network_thread_;
+	std::shared_ptr<LSY::DataFrame> data_frame_1_;
+	std::shared_ptr<LSY::ProtocolGenisysSlave> protocol_;
+	std::shared_ptr <LSY::NetworkGenisysSlave> network_;
+
 public:
 
 	cEvents();
-	void GridClickEvent(wxGridEvent& event) override;
+
+	// UI Events
 	void StartStopServerEvent(wxCommandEvent& event) override;
+	void GridClickEvent(wxGridEvent& event) override;
 	void MenuHelpAboutOnMenuSelection(wxCommandEvent& event) override;
-
-
 	void IdleEvent(wxIdleEvent& event);
 
+	// Network
+	bool StartupGenisysNetwork();
+	void ShutdownGenisysNetwork();
+	void RunningloopGenisysNetwork();
 
 };
